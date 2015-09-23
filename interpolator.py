9d7fe11b7f1bf63fe0a31303cff8b348428508e3
aubreychen9012 <aubrey9012@Aubreys-MacBook-Pro.local>
2015-09-23 17:02:32 +0200

    added copypaste interpolator

--- interpolator.py
+++ interpolator.py
@@ -3,6 +3,8 @@ import pandas as pd
 import math
 from scipy import interpolate
 import statsmodels.api as sm
+from itertools import groupby
+from operator import itemgetter
 from lasagne.layers import DenseLayer
 from lasagne.layers import InputLayer
 from lasagne.layers import DropoutLayer
@@ -70,19 +72,17 @@ def downsampling(data, threshold):
     return sampled
 
 def upsampling(data,upscale):
-    data_ = data[1:-1]
     x = range(len(data))
-    newx = [i/float(len(upscale)-2)*x[-1] for i in range(upscale-2)]
-    f = interpolate.interp1d(x,data_)
+    newx = [i/float(upscale)*x[-1] for i in range(upscale)]
+    f = interpolate.interp1d(x,data)
     new_data = f(newx)
+    new_data= new_data[1:-1]
     st = [data[0]]
     st.extend(new_data)
     st.append(data[-1])
     return st
 
 def build_feat(data, add_feat,window_length=20):
-    data = downsampling(data,len(data)/10)
-    data = np.asarray(data).T[1]
     ## use other available channels
     feat_mean = np.nan_to_num(pd.rolling_mean(data,5))
     feat_mean = signal_preproc.rolling_window(feat_mean, window_length)
@@ -101,7 +101,7 @@ def build_feat(data, add_feat,window_length=20):
     return feat
 
 def get_target(data,window_length=20):
-    target = signal_preproc.labels(signal_preproc.rolling_window(data,window_length=20))
+    target = signal_preproc.labels(signal_preproc.rolling_window(data,window_length))
     target = np.atleast_2d(np.asarray(target)).T
     target = target.astype('float32')
     return target
@@ -133,34 +133,39 @@ class NeuralnetInterpolator:
           predict_trend = upsampling(predict_trend,upscale = self.add_feat_pred)
           return predict_trend
 
-     def fit(self, tr,add_feat_tr):
+     def fit(self,tr,add_feat_tr):
           ## if trend exists, remove trend
           if self.trend ==1:
               trend = self.est_trend(tr)
               tr = tr-np.asarray(trend)
-          layers=[
-               (InputLayer, {'shape': (None,8, 2*self.window_length)}),
-               (DenseLayer, {'num_units': 8*2*self.window_length}),
+          layers0=[
+               ## 2 layers with one hidden layer
+               (InputLayer, {'shape': (None,8,self.window_length)}),
+               (DenseLayer, {'num_units': 8*self.window_length}),
                (DropoutLayer, {'p':0.3}),
-               (DenseLayer, {'num_units': 8*2*self.window_length/3}),
-               ]
-          feats = build_feat(tr, add_feat_tr, self.window_length)
-          feat_target = get_target(tr,self.window_length)
+               (DenseLayer, {'num_units': 8*self.window_length/3}),
+               ## the output layer
+               (DenseLayer, {'num_units': 1, 'nonlinearity': None}),
+          ]
+          feats = build_feat(tr, add_feat_tr, window_length=self.window_length)
+          print feats.shape
+          feat_target = get_target(tr,window_length=self.window_length)
+          print feat_target.shape
           net0 = NeuralNet(
-               layers=layers,
+               layers=layers0,
                max_epochs=400,
                update=nesterov_momentum,
                update_learning_rate=0.01,
                update_momentum=0.9,
                verbose=1,
                regression=True,
-               )
+          )
           net0.fit(feats[:-1],feat_target)
           return net0,feats,feat_target
 
      def validate(self,tr,add_feat_tr):
-         feats = build_feat(tr, add_feat_tr, self.window_length)
-         feat_target = get_target(tr,self.window_length)
+         feats = build_feat(tr, add_feat_tr, window_length=self.window_length)
+         feat_target = get_target(tr,window_length=self.window_length)
          net0,feats,feat_target = self.fit(tr,add_feat_tr)
          pred = clf.predict_feat(feats[0],net0,add_feat_tr,new_append_value=0,\
                                  window_length = self.window_length,lags = len(add_feat_tr)-1)
@@ -168,29 +173,86 @@ class NeuralnetInterpolator:
          return mse_error
 
      def interpolate(self):
-         tr = np.asarray(downsampling(self.tr,len(self.tr)/10)).T[1]
-         add_feat_tr = np.asarray(downsampling(self.add_feat_tr,len(self.add_feat_tr))).T[1]
-         add_feat_pred = np.asarray(downsampling(self.add_feat_pred,len(self.add_feat_pred))).T[1]
+         if len(self.add_feat_pred)/10<=2:
+             tr = np.asarray(self.tr)
+             add_feat_tr = np.asarray(self.add_feat_tr)
+             add_feat_pred = np.asarray(self.add_feat_pred)
+         else:
+             tr = np.asarray(downsampling(self.tr,len(self.tr)/10)).T[1]
+             add_feat_tr = np.asarray(downsampling(self.add_feat_tr,len(self.add_feat_tr)/10)).T[1]
+             add_feat_pred = np.asarray(downsampling(self.add_feat_pred,len(self.add_feat_pred)/10)).T[1]
+         print len(tr),len(add_feat_tr),len(add_feat_pred)
          net0,feats,feat_target=self.fit(tr,add_feat_tr)
          pred_res = clf.predict_feat(feats[-1],net0,add_feat_pred,new_append_value=0,\
-                                     window_length = self.window_length,lags = len(add_feat_pred)-1)
-         pred_res = upsampling(pred_res,upscale = len(self.add_feat_pred))
+                                     window_length = self.window_length,lags = len(add_feat_pred))
+         if len(self.add_feat_pred)/10>2:
+             pred_res = upsampling(pred_res,upscale = len(self.add_feat_pred))
          if self.trend==1:
              pred_res += self.trend_clf(tr,len(add_feat_pred))
          return pred_res
+         ## if MSE is nan, return 0 and pass it to copypaste
 
-class CopypasteInterpolator:
-     pass
+class CopyPasteInterpolator:
+    def __init__(self,sig):
+        if not isinstance(sig,np.ndarray):
+            sig = np.asarray(sig)
+        idx_nan_group = []
+        idx_nan = np.isnan(sig)
+        idx = np.arange(len(sig))
+        idx_nan_idx = idx[idx_nan]
+        for k, g in groupby(enumerate(idx_nan_idx), lambda (i,x):i-x):
+            idx_nan_group.append(map(itemgetter(1),g))
+        self.gaps= idx_nan_group
+        if self.gaps[0][0]==0:
+            self.gaps = self.gaps[1:]
+        self.sig = sig
+    def interpolate(self):
+        idx_nan_group = self.gaps
+        for group in idx_nan_group:
+            part_fw=[]
+            l = len(group)
+            if (l>1) and (l<len(self.sig)/2):
+                if group[0]<=l/2:
+                    part_bw = self.sig[group[-1]+1:group[-1]+1+l]
+                elif group[-1]+l/2+2>len(self.sig):
+                    part_bw = self.sig[group[0]-l:group[0]]
+                elif l%2:
+                    part_bw=self.sig[group[0]-l/2:group[0]]
+                    part_fw=self.sig[group[-1]+1:group[-1]+l/2+2]
+                elif not l%2:
+                    part_bw=self.sig[group[0]-l/2:group[0]]
+                    part_fw=self.sig[group[-1]+1:group[-1]+l/2+1]
+            elif l>=len(self.sig)/2:
+                part_bw=self.sig[:group[0]]
+                part_fw=self.sig[group[-1]+1:]
+                #sig[:group[0]+len(part_bw)]=part_bw
+                #sig[group[-1]-len(part_fw):]=part_fw
+            elif l==1:
+                part_bw = self.sig[group[0]-1] if group[0]!=0 else self.sig[group[0]+1]
+            if len(part_fw)>0:
+                part_bw= np.append(part_bw, part_fw)
+                if np.isnan(part_bw).any():
+                    part_bw = CopyPasteInterpolator(part_bw).interpolate()
+                if len(part_bw)!=len(group):
+                    print 'length unmatch'
+                    break
+            self.sig[group[0]:group[0]+l] = part_bw
+        new_sig = np.copy(self.sig)
+        return new_sig
+
 
 class SignalInterpolator:
-     def __init__(self,col,pf,window_length):
+     def __init__(self,col,pf,window_length,bool_var):
           self.col = col
           self.pf = pf
           self.window_length = window_length
+          self.bool_var = bool_var
 
      def interpolate(self):
           sig_main = preproc.get_column(self.col,self.pf)
           res = preproc.check(sig_main[2:],return_series=1)
+          if res[-1]>=0.9:
+              return sig_main
           sig_main_gaps = res[3]
           sig_array = res[0]
           active_chl = self.col
@@ -199,8 +261,8 @@ class SignalInterpolator:
                data = gen_add_feat.add_feat(rng,active_chl,self.pf)
                if data!=0:
                     tr, add_feat_tr,add_feat_pred = data[0],data[1],data[2]
-                    nn = NeuralnetInterpolator(tr,add_feat_tr,add_feat_pred,self.window_length)
-                    new_sig = nn.interpolate
+                    nn = NeuralnetInterpolator(tr,add_feat_tr,add_feat_pred,self.window_length,self.bool_var)
+                    new_sig = nn.interpolate()
                     sig_array[rng[0]:rng[-1]] = new_sig
                else:
                     pass
--- tx.py
+++ tx.py
@@ -2,15 +2,81 @@ __author__ = 'aubrey9012'
 
 import numpy as np
 import pandas as pd
+import pickle
 import sys
 
 sys.path.append('/Users/aubrey9012/Desktop')
 import preproc
 import interpolator
 
-col = 'ICP'
 file_name = '3270954'
 pf = '/Users/aubrey9012/Downloads/medical_ICU-features/data/mimic/'+str(file_name)+'.csv'
+header = preproc.get_header(pf)
+header.remove('Elapsed time')
 
-si = interpolator.SignalInterpolator(col,pf,40)
-si_ = si.interpolate()
+for col in header:
+    if col != 'ICP':
+        si = interpolator.SignalInterpolator(col,pf,40,0)
+    else:
+        si = interpolator.SignalInterpolator(col,pf,40,1)
+    si_  = si.interpolate()
+    if si_!=0:
+        cpi = interpolator.CopyPasteInterpolator(si_)
+        si_full = cpi.interpolate()
+        dump_name= str(col)+'.p'
+        pickle.dump(si_,open(dump_name, "wb" ))
+    if si_==0:
+        pickle.dump(si_,open(dump_name,"wb"))
+
+
+
+
+output_fname = '/Users/aubrey9012/Desktop/'+str(file_name)+'p.csv'
+output = open(output_fname,'wb')
+h = ['Elapsed time']
+h.extend(header)
+writer = csv.writer(output)
+writer.writerow(h)
+unit = ['seconds','mV','mV','mV','mV','mV','pm','NU','mmHg','mmHg']
+writer.writerow(unit)
+
+for i in range(len(time)):
+    timestamp = [time[i]]
+    timestamp.extend([sig_i.popleft(),sig_ii.popleft(),sig_iii.popleft(),sig_avr.popleft(),sig_v.popleft(),\
+                      sig_resp.popleft(),sig_pleth.popleft(),sig_abp.popleft(),sig_icp.popleft()])
+    writer.writerow(timestamp)
+output.close()
+
+f2 = open(str(col)+'.p','r')
+a = csv.reader(f2)
+
+with open(output_fname,'r+') as f1:
+    for line in f1:
+        line = line.strip()
+        line[1] = a.next()[0].split('F')[-1]
+
+        f1.write(line)
+
+
+
+for i in range(len(time)):
+    timestamp = [time[i]]
+    for col in header:
+        l=[]
+        val = reader(col).add()
+        if len(l)>0:
+            l.append(val)
+        else:
+            l = [val]
+    timestamp.extend(l)
+    print timestamp
+    writer.writerow(timestamp)
+
+output.close()
+
+
+
+
+
+
+
